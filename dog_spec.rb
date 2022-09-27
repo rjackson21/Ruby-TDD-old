e4a817d8ddf95bb0d164a78399a4244648743020
Regina Jackson <rjackson156@gmail.com>
2022-09-26 21:17:53 -0400

    test for bark

--- dog_spec.rb
+++ dog_spec.rb
@@ -1,1 +1,13 @@
+require 'rspec'
+require_relative 'dog'
+
+
+describe Dog do 
+    describe '#bark' do
+        it 'it returns the string "Woof!"' do
+            dog = Dog.new
+            expect(dog.bark).to eql('Woof!')
+        end
+    end
+end
 
