1631d4b7631fb7d77225841215c8d6385f621328
Regina Jackson <rjackson156@gmail.com>
2022-09-26 21:33:07 -0400

    build out hungry test

--- dog.rb
+++ dog.rb
@@ -1,5 +1,13 @@
 class Dog 
+    def initialize(hunger_level: 0)
+        @hunger_level = hunger_level
+    end
+
     def bark
         "Woof!"
+    end
+
+    def hungry
+        @hunger_level > 5 
     end
 end
--- dog_spec.rb
+++ dog_spec.rb
@@ -3,10 +3,23 @@ require_relative 'dog'
 
 
 describe Dog do 
+    subject { Dog.new }
+
     describe '#bark' do
         it 'it returns the string "Woof!"' do
-            dog = Dog.new
-            expect(dog.bark).to eql('Woof!')
+            expect(subject.bark).to eql('Woof!')
+        end
+    end
+
+    describe '#hungry' do
+        it 'returns true if hunger_level is more than 5' do
+            hungry_dog = Dog.new(hunger_level: 7)
+            expect(hungry_dog.hungry).to eql(true)
+        end
+
+        it 'returns false if hunger_level is 5 or less' do
+            hungry_dog = Dog.new(hunger_level: 5)
+            expect(hungry_dog.hungry).to eql(false)
         end
     end
 end
